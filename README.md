28e8413f3b1b4c935dcc30b765146e3850530397
sporritt <simon.porritt@gmail.com>
2013-12-18 08:51:25 +1100

    added biltong 0.2 and tests for the encloses method

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-jsplumb-geom
+biltong
 ============
 
-Geometry functions used internally by jsPlumb. Perhaps useful for other people. This is not intended to be an exhaustive list of functions: these are things that jsPlumb needs and nothing else.  In the interests of avoiding bloat in jsPlumb, it is unlikely that anything jsPlumb does not need will find its way into this project.
+Geometry functions used internally by jsPlumb. Perhaps useful for other people. This is not intended 
+to be an exhaustive list of functions: these are things that jsPlumb needs and nothing else.  In the 
+interests of avoiding bloat in jsPlumb, it is unlikely that anything jsPlumb does not need will find 
+its way into this project.
 
-jsPlumbGeom can be used both in the browser and on the server, and will register itself as a CommonJS module.
+Biltong can be used both in the browser and on the server, and will register itself as a CommonJS module.
 
 ### Usage
 
-All functions are in the global object `jsPlumbGeom`.  I haven't got around to setting up a build to process the API docs; check the source.
+All functions are in the global object `Biltong`.  I haven't got around to setting up a build to process the API docs; check the source.
--- src/biltong-0.2.js
+++ src/biltong-0.2.js
@@ -1,5 +1,5 @@
 /**
- * jsPlumbGeom v0.1
+ * Biltong v0.1
  *
  * Various geometry functions written as part of jsPlumb and perhaps useful for others.
  *
@@ -33,11 +33,11 @@
 
 	// Establish the root object, `window` in the browser, or `global` on the server.
 	var root = this;
-	var jsPlumbGeom;
+	var Biltong;
 	if (typeof exports !== 'undefined') {
-		jsPlumbGeom = exports;
+		Biltong = exports;
 	} else {
-		jsPlumbGeom = root.jsPlumbGeom = {};
+		Biltong = root.Biltong = {};
 	}
 
 	var _isa = function(a) { return Object.prototype.toString.call(a) === "[object Array]"; },
@@ -47,14 +47,14 @@
 		    return fn(p1, p2);
 		},
 		/**
-		* @name jsPlumbGeom.gradient
+		* @name Biltong.gradient
 		* @function
 		* @desc Calculates the gradient of a line between the two points.
 		* @param {Point} p1 First point, either as a 2 entry array or object with `left` and `top` properties.
 		* @param {Point} p2 Second point, either as a 2 entry array or object with `left` and `top` properties.
 		* @return {Float} The gradient of a line between the two points.
 		*/
-		_gradient = jsPlumbGeom.gradient = function(p1, p2) {
+		_gradient = Biltong.gradient = function(p1, p2) {
 		    return _pointHelper(p1, p2, function(_p1, _p2) { 
 		        if (_p2[0] == _p1[0])
 		            return _p2[1] > _p1[1] ? Infinity : -Infinity;
@@ -65,38 +65,38 @@
 		    });		
 		},
 		/**
-		* @name jsPlumbGeom.normal
+		* @name Biltong.normal
 		* @function
 		* @desc Calculates the gradient of a normal to a line between the two points.
 		* @param {Point} p1 First point, either as a 2 entry array or object with `left` and `top` properties.
 		* @param {Point} p2 Second point, either as a 2 entry array or object with `left` and `top` properties.
 		* @return {Float} The gradient of a normal to a line between the two points.
 		*/
-		_normal = jsPlumbGeom.normal = function(p1, p2) {
+		_normal = Biltong.normal = function(p1, p2) {
 		    return -1 / _gradient(p1, p2);
 		},
 		/**
-		* @name jsPlumbGeom.lineLength
+		* @name Biltong.lineLength
 		* @function
 		* @desc Calculates the length of a line between the two points.
 		* @param {Point} p1 First point, either as a 2 entry array or object with `left` and `top` properties.
 		* @param {Point} p2 Second point, either as a 2 entry array or object with `left` and `top` properties.
 		* @return {Float} The length of a line between the two points.
 		*/
-		_lineLength = jsPlumbGeom.lineLength = function(p1, p2) {
+		_lineLength = Biltong.lineLength = function(p1, p2) {
 		    return _pointHelper(p1, p2, function(_p1, _p2) {
 		        return Math.sqrt(Math.pow(_p2[1] - _p1[1], 2) + Math.pow(_p2[0] - _p1[0], 2));			
 		    });
 		},
 		/**
-		* @name jsPlumbGeom.quadrant
+		* @name Biltong.quadrant
 		* @function
 		* @desc Calculates the quadrant in which the angle between the two points lies. 
 		* @param {Point} p1 First point, either as a 2 entry array or object with `left` and `top` properties.
 		* @param {Point} p2 Second point, either as a 2 entry array or object with `left` and `top` properties.
 		* @return {Integer} The quadrant - 1 for upper right, 2 for lower right, 3 for lower left, 4 for upper left.
 		*/
-		_quadrant = jsPlumbGeom.quadrant = function(p1, p2) {
+		_quadrant = Biltong.quadrant = function(p1, p2) {
 		    return _pointHelper(p1, p2, function(_p1, _p2) {
 		        if (_p2[0] > _p1[0]) {
 		            return (_p2[1] > _p1[1]) ? 2 : 1;
@@ -110,14 +110,14 @@
 		    });
 		},
 		/**
-		* @name jsPlumbGeom.theta
+		* @name Biltong.theta
 		* @function
 		* @desc Calculates the angle between the two points. 
 		* @param {Point} p1 First point, either as a 2 entry array or object with `left` and `top` properties.
 		* @param {Point} p2 Second point, either as a 2 entry array or object with `left` and `top` properties.
 		* @return {Float} The angle between the two points.
 		*/
-		_theta = jsPlumbGeom.theta = function(p1, p2) {
+		_theta = Biltong.theta = function(p1, p2) {
 		    return _pointHelper(p1, p2, function(_p1, _p2) {
 		        var m = _gradient(_p1, _p2),
 		            t = Math.atan(m),
@@ -129,14 +129,14 @@
 		    });
 		},
 		/**
-		* @name jsPlumbGeom.intersects
+		* @name Biltong.intersects
 		* @function
 		* @desc Calculates whether or not the two rectangles intersect.
 		* @param {Rectangle} r1 First rectangle, as a js object in the form `{x:.., y:.., w:.., h:..}`
 		* @param {Rectangle} r2 Second rectangle, as a js object in the form `{x:.., y:.., w:.., h:..}`
 		* @return {Boolean} True if the rectangles intersect, false otherwise.
 		*/
-		_intersects = jsPlumbGeom.intersects = function(r1, r2) {
+		_intersects = Biltong.intersects = function(r1, r2) {
 		    var x1 = r1.x, x2 = r1.x + r1.w, y1 = r1.y, y2 = r1.y + r1.h,
 		        a1 = r2.x, a2 = r2.x + r2.w, b1 = r2.y, b2 = r2.y + r2.h;
 		
@@ -149,17 +149,33 @@
 			        ( (a1 <= x1 && x1 <= a2) && (b1 <= y2 && y2 <= b2) ) ||
 			        ( (a1 <= x2 && x1 <= a2) && (b1 <= y2 && y2 <= b2) );
 		},
+		/**
+		* @name Biltong.encloses
+		* @function
+		* @desc Calculates whether or not r2 is completely enclosed by r1.
+		* @param {Rectangle} r1 First rectangle, as a js object in the form `{x:.., y:.., w:.., h:..}`
+		* @param {Rectangle} r2 Second rectangle, as a js object in the form `{x:.., y:.., w:.., h:..}`
+		* @param {Boolean} [allowSharedEdges=false] If true, the concept of enclosure allows for one or more edges to be shared by the two rectangles.
+		* @return {Boolean} True if r1 encloses r2, false otherwise.
+		*/
+		_encloses = Biltong.encloses = function(r1, r2, allowSharedEdges) {
+			var x1 = r1.x, x2 = r1.x + r1.w, y1 = r1.y, y2 = r1.y + r1.h,
+		        a1 = r2.x, a2 = r2.x + r2.w, b1 = r2.y, b2 = r2.y + r2.h,
+				c = function(v1, v2, v3, v4) { return allowSharedEdges ? v1 <= v2 && v3>= v4 : v1 < v2 && v3 > v4; };
+				
+			return c(x1,a1,x2,a2) && c(y1,b1,y2,b2);
+		},
 		_segmentMultipliers = [null, [1, -1], [1, 1], [-1, 1], [-1, -1] ],
 		_inverseSegmentMultipliers = [null, [-1, -1], [-1, 1], [1, 1], [1, -1] ],
 		/**
-		* @name jsPlumbGeom.pointOnLine
+		* @name Biltong.pointOnLine
 		* @function
 		* @desc Calculates a point on the line from `fromPoint` to `toPoint` that is `distance` units along the length of the line.
 		* @param {Point} p1 First point, either as a 2 entry array or object with `left` and `top` properties.
 		* @param {Point} p2 Second point, either as a 2 entry array or object with `left` and `top` properties.
 		* @return {Point} Point on the line, in the form `{ x:..., y:... }`.
 		*/
-		_pointOnLine = jsPlumbGeom.pointOnLine = function(fromPoint, toPoint, distance) {
+		_pointOnLine = Biltong.pointOnLine = function(fromPoint, toPoint, distance) {
 		    var m = _gradient(fromPoint, toPoint),
 		        s = _quadrant(fromPoint, toPoint),
 		        segmentMultiplier = distance > 0 ? _segmentMultipliers[s] : _inverseSegmentMultipliers[s],
@@ -169,14 +185,14 @@
 		    return { x:fromPoint.x + x, y:fromPoint.y + y };
 		},
 		/**
-		* @name jsPlumbGeom.perpendicularLineTo
+		* @name Biltong.perpendicularLineTo
 		* @function
 		* @desc Calculates a line of length `length` that is perpendicular to the line from `fromPoint` to `toPoint` and passes through `toPoint`.
 		* @param {Point} p1 First point, either as a 2 entry array or object with `left` and `top` properties.
 		* @param {Point} p2 Second point, either as a 2 entry array or object with `left` and `top` properties.
 		* @return {Line} Perpendicular line, in the form `[ { x:..., y:... }, { x:..., y:... } ]`.
 		*/        
-		_perpendicularLineTo = jsPlumbGeom.perpendicularLineTo = function(fromPoint, toPoint, length) {
+		_perpendicularLineTo = Biltong.perpendicularLineTo = function(fromPoint, toPoint, length) {
 		    var m = _gradient(fromPoint, toPoint),
 		        theta2 = Math.atan(-1 / m),
 		        y =  length / 2 * Math.sin(theta2),
